93628d9ffe502efa9b4404213e73744e6c40261b
Ennis Massey <ennisbaradine@gmail.com>
2018-02-11 20:27:02 +1300

    Implemented __or__ for column specifiers

--- wikiman/groff/structures.py
+++ wikiman/groff/structures.py
@@ -4,27 +4,98 @@ Provides an abstracted form of various structures in their Groff form
 
 from typing import List, Dict, Union
 from abc import ABCMeta, abstractclassmethod, abstractproperty, abstractmethod
-from enum import Enum
+from enum import Enum, Flag, auto, _decompose
 from textwrap import dedent
+from itertools import chain
 
 
 # == Typing stuff == #
 class HeaderLevels(Enum):
+    """
+    Set of usable header types within Groff using MS macros
+    """
     HEADER_LARGE = '.SH 1'
     HEADER_MEDIUM = '.SH 2'
     HEADER_SMALL = '.SH 3'
     HEADER_CUSTOM = '.SH {size}'
     NUMBERED_HEADER = '.NH {size} {section}'
 
+class GroffColumnSpecifiers(Flag):
+    """
+    Flags for easy combinations of Unix tbl column specifiers
+    """
+    RELATIVE_CENTER = auto()
+    CENTER = auto()
+    LEFT_ALIGNED = auto()
+    RIGHT_ALIGNED = auto()
+    NUMBER_ALIGNED = auto()
+    VERTICAL_SPAN = auto()
+    HORIZONTAL_SPAN = auto()
+
+    _uncombinable = (RELATIVE_CENTER|CENTER|LEFT_ALIGNED|RIGHT_ALIGNED)
+
+    def __or__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        self_components = _decompose(self.__class__, self._value_)[0]
+        other_components = _decompose(self.__class__, other._value_)[0]
+        self_primary = list(filter(lambda x: x in self._uncombinable, self_components))[:1][0]
+        other_combinable = list(filter(lambda x: x not in self._uncombinable, other_components))
+        print(dedent("""\
+        First Argument: {}
+        Second Argument: {}
+        Self Primary: {}
+        Other Combinable: {}
+        """.format(
+            self_components,
+            other_components,
+            self_primary,
+            other_combinable
+        )))
+        usable_output = self_primary
+        for val in other_combinable:
+            usable_output = Flag.__or__(usable_output, val)
+        usable_output._name_ = None if len(other_combinable) > 0 else usable_output._name_
+        return usable_output
+
+
+class GroffColumnOptions(Flag):
+    """
+    Flags for options in table columns which affect presentation of column content
+    """
+    BOLD = auto()
+    ITALIC = auto()
+    VERTICAL_SPAN_START_BOTTOM = auto()
+    VERTICAL_SPAN_START_TOP = auto()
+    UP_HALF_LINE = auto()
+    EXPAND_COLUMN = auto()
+    IGNORE_COLUMN = auto()
+
+
+
+
 
 class GroffTextEntity(metaclass=ABCMeta):
+    """
+    An abstract class representing any solid concept or entity present in Groff when used with the MS macro set
+    """
+
     @property
     @abstractmethod
     def text(self) -> str:
+        """
+        Should return a fully usable version of the entity
+
+        Returns:
+            str: Usable textual version of entity
+        """
         pass
 
 
-class GroffTable:
+class GroffTable(GroffTextEntity):
+    """
+
+    """
     def __init__(self):
         pass
 
@@ -40,6 +111,14 @@ class GroffTable:
     @property
     def text(self) -> str:
         pass
+
+class GroffTableRow(GroffTextEntity):
+    def __init__(self):
+        pass
+
+
+
+
 
 
 class GroffLinebreak:
@@ -71,7 +150,7 @@ class GroffColouredText(GroffTextEntity):
 
     @property
     def text(self) -> str:
-        return "\\m[{self.colour}]{self.raw_text}\\m[]".format(self=self)
+        return "\m[{self.colour}]{self.raw_text}\m[]".format(self=self)
 
 class GroffHeader(GroffTextEntity):
     """
